5604c9a6d6708ff1a98501633204d37dd0626704
Fred Hartley <fred@taylorthomas.co.uk>
2023-11-12 13:19:06 +0000

    further styling and reimplemented 5 round validation

--- scripts.js
+++ scripts.js
@@ -21,37 +21,22 @@
         // Function 2 — playRound
         // Define function called playRound
         function playRound(playerSelection, computerSelection) { 
-            // const userInput = prompt("Rock, paper or scissors?");
-            // // Convert to uppercase for validation purposes
-            // const userInputUpper = userInput.toUpperCase();
-            // // Validate initial answer from user and return answer or remind them to only input correct strings
-            // if (userInputUpper === "ROCK" || 
-            //     userInputUpper === "PAPER" || 
-            //     userInputUpper === "SCISSORS") {
-            //     playerSelection = userInputUpper;
-            // }   else {
-            //     alert("Input rock, paper or scissors.");
-            // // Recall the function if the incorrect input is made
-            //     return playRound();
-            // }  
             // Compare playerSelection vs computerSelection 
             // If tied, no one wins
             if (playerSelection === computerSelection) {
-            document.getElementById("results-text").textContent = `It is a tie. Computer ${computerScore} - Player ${playerScore}`
-            console.log(`It is a tie. Computer ${computerScore} - Player ${playerScore}`); 
+            document.getElementById("results-text").textContent = `It is a tie. You ${playerScore} – Officer Raskalov ${computerScore}`
+            console.log(`It is a tie. You ${playerScore} – Officer Raskalov ${computerScore}`); 
 
             // Set various different scenarios of player winning and add 1 to score
         }   else if ((playerSelection === "PAPER" && computerSelection === "ROCK") || 
                      (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
                      (playerSelection === "SCISSORS" && computerSelection === "PAPER")) {
                     playerScore++;
-                    document.getElementById("results-text").textContent = `Player wins. Player ${playerScore} - Computer ${computerScore}`
-                    console.log(`Player wins. Player ${playerScore} - Computer ${computerScore}`);
+                    document.getElementById("results-text").textContent = `You win. You ${playerScore} - Officer Raskalov ${computerScore}`
             // Any other possibility leaves to computer winning so add 1 to score
         }   else {
                     computerScore++;
-                    document.getElementById("results-text").textContent = `Computer wins. Player ${playerScore} - Computer ${computerScore}`
-                    console.log(`Computer wins. Player ${playerScore} - Computer ${computerScore}`);  
+                    document.getElementById("results-text").textContent = `Officer Raskalov wins. You ${playerScore} - Officer Raskalov ${computerScore}`
         }   
         } 
 
@@ -60,19 +45,45 @@
         roundChoice.addEventListener('click', (event) => {
             let target = event.target;
         
+            if (roundNumber < 5) {
             switch(target.id) {
                 case 'rock-pic':
                     console.log('Rock was clicked');
                     playRound("ROCK", getComputerChoice())
+                    roundNumber++;
+                    console.log(roundNumber);
                     break;
                 case 'paper-pic':
                     console.log('Paper was clicked');
                     playRound("PAPER", getComputerChoice())
+                    roundNumber++;
+                    console.log(roundNumber);
                     break;
                 case 'scissors-pic':
                     console.log('Scissors was clicked');
                     playRound("SCISSORS", getComputerChoice())
+                    roundNumber++;
+                    console.log(roundNumber);
                     break;
+            }
+        }
+            else {
+                // If player's score is bigger, alert "You win"
+                if (playerScore > computerScore) {
+                    document.getElementById("results-text").textContent = `Final Score: You ${playerScore} - Officer Raskalov ${computerScore}. You win.`;
+                    document.getElementById("extra-dialogue").textContent = `'I'll be watching you.' snarls Officer Raskalov as he storms out the room.`;
+                // If player's score is smaller, alert "You lose"
+                } else if (playerScore < computerScore) {
+                    document.getElementById("results-text").textContent = `Final Score: You ${playerScore} - Officer Raskalov ${computerScore}. You lose...`;
+                    document.getElementById("extra-dialogue").textContent = `'HAHA! Face the wall!' Officer Raskalov sadistically announces.`;
+                // Anything else has to be a tie
+                } else {
+                    document.getElementById("results-text").textContent = `Final Score: You ${playerScore} - Officer Raskalov ${computerScore}. It's a tie.`;
+                    document.getElementById("extra-dialogue").textContent = `'Again!' screams Officer Raskalov.`;
+                    playerScore = 0;
+                    computerScore = 0;
+                    roundNumber = 0;
+                }
             }
         });
 
