98973c15ffc2a0aa85b2621d70ccefbeed28af14
Fred Hartley <fred@taylorthomas.co.uk>
2023-11-16 14:29:08 +0000

    Broke story into stages with if statement in click event listener

--- scripts.js
+++ scripts.js
@@ -1,123 +1,151 @@
+// create variable for current stage
+let currentStage = 1;
+
+// create function to replace image src and alt
+function imageReplace(imageVar, imageSrc, imageAlt) {
+    imageVar.src = imageSrc;
+    imageVar.alt = imageAlt;
+}
+// create event listener to start game
 document.addEventListener("DOMContentLoaded", function() {
-    const removeButton = document.getElementById("removeButton");
+    // Gets ID's of existing elements on the page
+    const stageImg = document.getElementById("stageImg");
     const titleText = document.getElementById("titleText");
-    const introDiv = document.getElementById("introDiv");
-    const introParaOne = document.createElement("p");
-    const nextBtn = document.createElement("button");
-    const imgStageOne = document.createElement("img");
+    const storyText = document.getElementById("storyText");
+    const startButton = document.getElementById("startButton");
 
-    introParaOne.textContent = "The year is 1933, in the frosty grip of a Soviet winter, desperation and hunger clawed at the hearts of the people. You, a humble soul driven by the instinct to protect your family, found yourself on the wrong side of the law after stealing a single loaf of bread from the desolate bakery. The Soviet famine had spared no one, and your family's survival depended on that stolen sustenance.";
-    nextBtn.textContent = "Next";
-    imgStageOne.src = "images/stage-one-img.jpeg";
-    imgStageOne.alt = "An illustration of people pleading for bread from a stand in Russia";
-
-    removeButton.addEventListener("click", function() {
-        titleText.remove();
-        removeButton.remove();
-        introDiv.appendChild(imgStageOne);
-        introDiv.appendChild(introParaOne);
-        introDiv.appendChild(nextBtn);
-    });
-});
-
-
-
-
-
-
-
-
+    startButton.addEventListener("click", function() {
+        if (currentStage === 1) {
+            titleText.remove();
+            storyText.textContent = "The year is 1933, in the frosty grip of a Soviet winter, desperation and hunger clawed at the hearts of the people. You, a humble soul driven by the instinct to protect your family, found yourself on the wrong side of the law after stealing a single loaf of bread from the desolate bakery. The Soviet famine had spared no one, and your family's survival depended on that stolen sustenance.";
+            imageReplace(stageImg, "images/stage-one-img.jpeg", "An illustration of people pleading for bread from a stand in Russia");
+            startButton.textContent = "Next";
+            currentStage++;
+            console.log(currentStage);
+        } else if (currentStage === 2) {
+            storyText.textContent = "Enter the stage, a local corrupt police officer, a man who reveled in the misfortune of others. He caught you red-handed, with the stolen bread as damning evidence. Instead of turning you in immediately, he presented a sinister proposition. 'Beat me at a game of Rock, Paper, Scissors, and I'll turn a blind eye to your transgressions. Fail, and you and your family face the firing squad.'";
+            imageReplace(stageImg, "images/stage-two-img.jpeg", "A illustration of an angry Russian officer.");
+            currentStage++;
+            console.log(currentStage);
+        } else if (currentStage === 3) {
+            storyText.textContent = "The chilling wind whipped through the desolate streets as you reluctantly agreed, knowing that the stakes were life and death. The corrupt officer grinned, the icy glint in his eye reflecting the stark reality of your situation.";
+            imageReplace(stageImg, "images/stage-three-img.jpeg", "A illustration of two hands aggressively interlocking on a red background.");
+            currentStage++;
+            console.log(currentStage);
+        }
+    })
+}
+)
 
 // Code for game mechanics
 // Counter for score 
-        let playerScore = 0;
-        let computerScore = 0;
-        let roundNumber = 0;
-        let playerSelection;
-        // Function 1 – getComputerChoice
-        // Define function called getComputerChoice
-        function getComputerChoice() {
-        // Create a int variable to store the choice with an initVal of null 
-        // Randomly generate a whole number between 1 and 3 
-            let randomNumber = Math.floor((Math.random() * 3) + 1);
-        // Create a conditional statement — if 1, 2 and 3 assign respectively to "ROCK", "PAPER" or "SCISSORS"
-            if (randomNumber === 1) {
-                return "ROCK";
-            } else if (randomNumber === 2) {
-                return "PAPER";
-            } else
-                return "SCISSORS";
-        }
-        // Function 2 — playRound
-        // Define function called playRound
-        function playRound(playerSelection, computerSelection) { 
-            // Compare playerSelection vs computerSelection 
-            // If tied, no one wins
-            if (playerSelection === computerSelection) {
-            document.getElementById("results-text").textContent = `It is a tie. You ${playerScore} – Officer Raskalov ${computerScore}`
-            console.log(`It is a tie. You ${playerScore} – Officer Raskalov ${computerScore}`); 
+        // let playerScore = 0;
+        // let computerScore = 0;
+        // let roundNumber = 0;
+        // let playerSelection;
+        // // Function 1 – getComputerChoice
+        // // Define function called getComputerChoice
+        // function getComputerChoice() {
+        // // Create a int variable to store the choice with an initVal of null 
+        // // Randomly generate a whole number between 1 and 3 
+        //     let randomNumber = Math.floor((Math.random() * 3) + 1);
+        // // Create a conditional statement — if 1, 2 and 3 assign respectively to "ROCK", "PAPER" or "SCISSORS"
+        //     if (randomNumber === 1) {
+        //         return "ROCK";
+        //     } else if (randomNumber === 2) {
+        //         return "PAPER";
+        //     } else
+        //         return "SCISSORS";
+        // }
+        // // Function 2 — playRound
+        // // Define function called playRound
+        // function playRound(playerSelection, computerSelection) { 
+        //     // Compare playerSelection vs computerSelection 
+        //     // If tied, no one wins
+        //     if (playerSelection === computerSelection) {
+        //     document.getElementById("results-text").textContent = `It is a tie. You ${playerScore} – Officer Raskalov ${computerScore}`
+        //     console.log(`It is a tie. You ${playerScore} – Officer Raskalov ${computerScore}`); 
 
-            // Set various different scenarios of player winning and add 1 to score
-        }   else if ((playerSelection === "PAPER" && computerSelection === "ROCK") || 
-                     (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
-                     (playerSelection === "SCISSORS" && computerSelection === "PAPER")) {
-                    playerScore++;
-                    document.getElementById("results-text").textContent = `You win. You ${playerScore} - Officer Raskalov ${computerScore}`
-            // Any other possibility leaves to computer winning so add 1 to score
-        }   else {
-                    computerScore++;
-                    document.getElementById("results-text").textContent = `Officer Raskalov wins. You ${playerScore} - Officer Raskalov ${computerScore}`
-        }   
-        } 
+        //     // Set various different scenarios of player winning and add 1 to score
+        // }   else if ((playerSelection === "PAPER" && computerSelection === "ROCK") || 
+        //              (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+        //              (playerSelection === "SCISSORS" && computerSelection === "PAPER")) {
+        //             playerScore++;
+        //             document.getElementById("results-text").textContent = `You win. You ${playerScore} - Officer Raskalov ${computerScore}`
+        //     // Any other possibility leaves to computer winning so add 1 to score
+        // }   else {
+        //             computerScore++;
+        //             document.getElementById("results-text").textContent = `Officer Raskalov wins. You ${playerScore} - Officer Raskalov ${computerScore}`
+        // }   
+        // } 
 
-        const roundChoice = document.querySelector('#round-selection');
+        // const roundChoice = document.querySelector('#round-selection');
 
-        roundChoice.addEventListener('click', (event) => {
-            let target = event.target;
+        // roundChoice.addEventListener('click', (event) => {
+        //     let target = event.target;
         
-            if (roundNumber < 5) {
-            switch(target.id) {
-                case 'rock-pic':
-                    console.log('Rock was clicked');
-                    playRound("ROCK", getComputerChoice())
-                    roundNumber++;
-                    console.log(roundNumber);
-                    break;
-                case 'paper-pic':
-                    console.log('Paper was clicked');
-                    playRound("PAPER", getComputerChoice())
-                    roundNumber++;
-                    console.log(roundNumber);
-                    break;
-                case 'scissors-pic':
-                    console.log('Scissors was clicked');
-                    playRound("SCISSORS", getComputerChoice())
-                    roundNumber++;
-                    console.log(roundNumber);
-                    break;
-            }
-        }
-            else {
-                // If player's score is bigger, alert "You win"
-                if (playerScore > computerScore) {
-                    document.getElementById("results-text").textContent = `Final Score: You ${playerScore} - Officer Raskalov ${computerScore}. You win.`;
-                    document.getElementById("extra-dialogue").textContent = `'I'll be watching you.' snarls Officer Raskalov as he storms out the room.`;
-                // If player's score is smaller, alert "You lose"
-                } else if (playerScore < computerScore) {
-                    document.getElementById("results-text").textContent = `Final Score: You ${playerScore} - Officer Raskalov ${computerScore}. You lose...`;
-                    document.getElementById("extra-dialogue").textContent = `'HAHA! Face the wall!' Officer Raskalov sadistically announces.`;
-                // Anything else has to be a tie
-                } else {
-                    document.getElementById("results-text").textContent = `Final Score: You ${playerScore} - Officer Raskalov ${computerScore}. It's a tie.`;
-                    document.getElementById("extra-dialogue").textContent = `'Again!' screams Officer Raskalov.`;
-                    playerScore = 0;
-                    computerScore = 0;
-                    roundNumber = 0;
-                }
-            }
-        });
+        //     if (roundNumber < 5) {
+        //     switch(target.id) {
+        //         case 'rock-pic':
+        //             console.log('Rock was clicked');
+        //             playRound("ROCK", getComputerChoice())
+        //             roundNumber++;
+        //             console.log(roundNumber);
+        //             break;
+        //         case 'paper-pic':
+        //             console.log('Paper was clicked');
+        //             playRound("PAPER", getComputerChoice())
+        //             roundNumber++;
+        //             console.log(roundNumber);
+        //             break;
+        //         case 'scissors-pic':
+        //             console.log('Scissors was clicked');
+        //             playRound("SCISSORS", getComputerChoice())
+        //             roundNumber++;
+        //             console.log(roundNumber);
+        //             break;
+        //     }
+        // }
+        //     else {
+        //         // If player's score is bigger, alert "You win"
+        //         if (playerScore > computerScore) {
+        //             document.getElementById("results-text").textContent = `Final Score: You ${playerScore} - Officer Raskalov ${computerScore}. You win.`;
+        //             document.getElementById("extra-dialogue").textContent = `'I'll be watching you.' snarls Officer Raskalov as he storms out the room.`;
+        //         // If player's score is smaller, alert "You lose"
+        //         } else if (playerScore < computerScore) {
+        //             document.getElementById("results-text").textContent = `Final Score: You ${playerScore} - Officer Raskalov ${computerScore}. You lose...`;
+        //             document.getElementById("extra-dialogue").textContent = `'HAHA! Face the wall!' Officer Raskalov sadistically announces.`;
+        //         // Anything else has to be a tie
+        //         } else {
+        //             document.getElementById("results-text").textContent = `Final Score: You ${playerScore} - Officer Raskalov ${computerScore}. It's a tie.`;
+        //             document.getElementById("extra-dialogue").textContent = `'Again!' screams Officer Raskalov.`;
+        //             playerScore = 0;
+        //             computerScore = 0;
+        //             roundNumber = 0;
+        //         }
+        //     }
+        // });
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// OLD
 
 // Code for game mechanics
 // // Counter for score 
